7420ac0b1ed5b0d08c245f4fabb3d797cb0b2211
Muhammad Hassaan <hassaanmajeed85@gmail.com>
2025-02-27 11:56:57 +0500

    Update OTA.h

--- include/OTA.h
+++ include/OTA.h
@@ -6,7 +6,7 @@
 #include "config.h"
 
 #define AWS_S3_BUCKET "aqi-firemware-updates"
-#define AWS_S3_REGION "eu-central-1Europe (Stockholm) eu-north-1"
+#define AWS_S3_REGION "eu-north-1"
 #define OTA_UPDATE_FAILURE_LIMIT 3
 
 #define CLOUD_HOST_URL "https://" AWS_S3_BUCKET ".s3." AWS_S3_REGION ".amazonaws.com/aqi/firmware_"
